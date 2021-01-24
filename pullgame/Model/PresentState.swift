553e0ca218e6584ee4f4d0e536a0401b97fafe61
akio0911 <akio0911@gmail.com>
2021-01-24 18:18:10 +0900

    ゲームの状態を表すための型を追加

--- pullgame/Model/PresentState.swift
+++ pullgame/Model/PresentState.swift
@@ -52,3 +52,54 @@ class PresentLocation {
     
     
 }
+
+protocol ItemType {
+    var imageName: String { get }
+    var score: Int { get }
+    var isBomb: Bool { get }
+}
+
+struct Apple: ItemType {
+    let imageName = "apple"
+    let score = 10
+    var isBomb = false
+}
+
+struct Bomb: ItemType {
+    let imageName = "bomb"
+    let score = -50
+    var isBomb = true
+}
+
+enum Player {
+    case player1
+    case player2
+}
+
+enum ItemBeltPosition {
+    case pos0, pos1, pos2, pos3, pos4
+    
+    static let center = ItemBeltPosition.pos2
+}
+
+enum ItemPosition {
+    case onBelt(ItemBeltPosition)
+    case outOfBelt(Player)
+}
+
+struct BeltState {
+    let item: ItemType
+    let itemPosition: ItemPosition
+}
+
+var beltStates: [BeltState] = [
+    BeltState(item: Apple(), itemPosition: .onBelt(.center)),
+    BeltState(item: Bomb(), itemPosition: .onBelt(.center)),
+    BeltState(item: Apple(), itemPosition: .onBelt(.center)),
+    BeltState(item: Bomb(), itemPosition: .onBelt(.center)),
+    BeltState(item: Apple(), itemPosition: .onBelt(.center)),
+]
+
+func configureUI(beltStates: [BeltState]) {
+    // ここでUIを適切に設定する
+}
