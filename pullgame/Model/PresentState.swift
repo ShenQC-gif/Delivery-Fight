4b23209237c2af4ddd5af93dd3827346de358364
akio0911 <akio0911@gmail.com>
2021-01-24 18:37:23 +0900

    didTapDown追加

--- pullgame/Model/PresentState.swift
+++ pullgame/Model/PresentState.swift
@@ -80,6 +80,26 @@ enum ItemBeltPosition {
     case pos0, pos1, pos2, pos3, pos4
     
     static let center = ItemBeltPosition.pos2
+    
+    func next() -> ItemBeltPosition? {
+        switch self {
+        case .pos0: return .pos1
+        case .pos1: return .pos2
+        case .pos2: return .pos3
+        case .pos3: return .pos4
+        case .pos4: return nil
+        }
+    }
+    
+    func prev() -> ItemBeltPosition? {
+        switch self {
+        case .pos0: return nil
+        case .pos1: return .pos0
+        case .pos2: return .pos1
+        case .pos3: return .pos2
+        case .pos4: return .pos3
+        }
+    }
 }
 
 enum ItemPosition {
@@ -92,14 +112,36 @@ struct BeltState {
     let itemPosition: ItemPosition
 }
 
-var beltStates: [BeltState] = [
-    BeltState(item: Apple(), itemPosition: .onBelt(.center)),
-    BeltState(item: Bomb(), itemPosition: .onBelt(.center)),
-    BeltState(item: Apple(), itemPosition: .onBelt(.center)),
-    BeltState(item: Bomb(), itemPosition: .onBelt(.center)),
-    BeltState(item: Apple(), itemPosition: .onBelt(.center)),
-]
+func makeInitialState() -> [BeltState] {
+    [
+        BeltState(item: Apple(), itemPosition: .onBelt(.center)),
+        BeltState(item: Bomb(), itemPosition: .onBelt(.center)),
+        BeltState(item: Apple(), itemPosition: .onBelt(.center)),
+        BeltState(item: Bomb(), itemPosition: .onBelt(.center)),
+        BeltState(item: Apple(), itemPosition: .onBelt(.center)),
+    ]
+}
 
 func configureUI(beltStates: [BeltState]) {
     // ここでUIを適切に設定する
+}
+
+func didTapDown(tappingPlayer: Player, beltState: BeltState) -> BeltState {
+    let newItemPosition: ItemPosition
+    
+    switch beltState.itemPosition {
+    case let .onBelt(position):
+        switch tappingPlayer {
+        case .player1:
+            newItemPosition = position.next().map { ItemPosition.onBelt($0) }
+                  ?? ItemPosition.outOfBelt(.player1)
+        case .player2:
+            newItemPosition = position.prev().map { ItemPosition.onBelt($0) }
+                  ?? ItemPosition.outOfBelt(.player2)
+        }
+    case .outOfBelt:
+        newItemPosition = beltState.itemPosition
+    }
+
+    return BeltState(item: beltState.item, itemPosition: newItemPosition)
 }
