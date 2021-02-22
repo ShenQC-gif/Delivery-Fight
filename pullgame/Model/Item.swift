da95c70a6aefe0606c9b3a3df185e9accf9e03e2
QiChen <shen24qichen@gmail.com>
2021-02-22 19:42:04 +0900

    モデルを別ファイルに分離

--- pullgame/Model/Item.swift
+++ pullgame/Model/Item.swift
@@ -62,44 +62,6 @@ struct Bomb: ItemType {
     var isBomb = true
 }
 
-enum Player {
-    case player1
-    case player2
-}
 
-enum ItemBeltPosition {
-    case pos0, pos1, pos2, pos3, pos4
-    
-    static let center = ItemBeltPosition.pos2
-    
-    func next() -> ItemBeltPosition? {
-        switch self {
-        case .pos0: return .pos1
-        case .pos1: return .pos2
-        case .pos2: return .pos3
-        case .pos3: return .pos4
-        case .pos4: return nil
-        }
-    }
-    
-    func prev() -> ItemBeltPosition? {
-        switch self {
-        case .pos0: return nil
-        case .pos1: return .pos0
-        case .pos2: return .pos1
-        case .pos3: return .pos2
-        case .pos4: return .pos3
-        }
-    }
-}
 
-enum ItemPosition: Equatable {
-    case onBelt(ItemBeltPosition)
-    case outOfBelt(Player)
-}
-
-struct BeltState {
-    let item: ItemType
-    let itemPosition: ItemPosition
-}
 
