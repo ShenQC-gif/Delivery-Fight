19589b0814eea20ccf19af08a16d5055d3566751
QiChen <shen24qichen@gmail.com>
2021-03-14 08:39:40 +0900

    letに変更

--- pullgame/Model/Item.swift
+++ pullgame/Model/Item.swift
@@ -17,47 +17,47 @@ protocol ItemType {
 struct Apple: ItemType {
     let imageName = "apple"
     let score = 10
-    var isBomb = false
+    let isBomb = false
 }
 
 struct Grape: ItemType {
     let imageName = "grape"
     let score = 20
-    var isBomb = false
+    let isBomb = false
 }
 
 struct Melon: ItemType {
     let imageName = "melon"
     let score = 30
-    var isBomb = false
+    let isBomb = false
 }
 
 struct Peach: ItemType {
     let imageName = "peach"
     let score = 40
-    var isBomb = false
+    let isBomb = false
 }
 
 struct Banana: ItemType {
     let imageName = "banana"
     let score = 50
-    var isBomb = false
+    let isBomb = false
 }
 
 struct Cherry: ItemType {
     let imageName = "cherry"
     let score = 60
-    var isBomb = false
+    let isBomb = false
 }
 
 struct Diamond: ItemType {
     let imageName = "diamond"
     let score = 100
-    var isBomb = false
+    let isBomb = false
 }
 
 struct Bomb: ItemType {
     let imageName = "bomb"
     let score = -50
-    var isBomb = true
+    let isBomb = true
 }
